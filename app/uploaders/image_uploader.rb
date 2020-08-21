b69d1330b24ee096bdfbe34fcaf646bdb5d59998
taihei.kuma <tai.kuma@gmail.com>
2020-08-21 11:58:44 +0000

    original

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
+  #if Rails.env.production?
      include Cloudinary::CarrierWave
-    else
-      storage :file
-  end
+   # else
+    #  storage :file
+  #end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
